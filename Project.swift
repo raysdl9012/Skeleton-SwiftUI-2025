efd7eb45a2fe1d980f942829cbf868fc827cee37
Reinner Steven Daza Leiva <reinner.leiva@outlook.com>
2025-11-05 17:03:26 -0500

    skeleton

--- Project.swift
+++ Project.swift
@@ -3,32 +3,24 @@ import ProjectDescription
 
 // MARK: - Base Settings
 
-private let name = "Project"
+private let name = "InstagramClone"
 private let bundleIdentifier = "com.rsdl.project"
 private let organization = "rsdl"
 
 let baseSettings: SettingsDictionary = [:]
 
-func debugSettings() -> SettingsDictionary {
-    let settings = baseSettings
-    return settings
-}
 
-func stagingSettings() -> SettingsDictionary {
-    var settings = baseSettings
-    return settings
-}
-
-func releaseSettings() -> SettingsDictionary {
-    var settings = baseSettings
-    return settings
-}
 
 // MARK: - Project Definition
 let project = Project(
     name: "\(name)",
-    organizationName: "\(organization)",
+    organizationName: "\(organization)", options: .options(
+        automaticSchemesOptions: .enabled(
+            codeCoverageEnabled: true
+        )
+    ),
     settings: .settings(
+        base: getBasicSettings(),
         configurations: [
             .debug(name: "Debug",
                    settings: debugSettings(),
@@ -48,30 +40,22 @@ let project = Project(
             product: .app,
             bundleId: "\(bundleIdentifier)",
             infoPlist: .extendingDefault(
-                with: [
-                    "UILaunchScreen": [
-                        "UIColorName": "",
-                        "UIImageName": "",
-                    ],
-                ]
+                with: getExtenseInfoPlist()
             ),
-            buildableFolders: [
-                "Project/Sources",
-                "Project/Resources",
-            ],
+            buildableFolders: getBuildableFolders(),
             dependencies: []
         ),
         .target(
-            name: "ProjectTests",
+            name: "\(name)Tests",
             destinations: .iOS,
             product: .unitTests,
             bundleId: "\(bundleIdentifier)Tests",
             infoPlist: .default,
             buildableFolders: [
-                "Project/Tests"
+                .init(stringLiteral:"\(name)/Tests")
             ],
             dependencies: [
-                .target(name: "Project")
+                .target(name: "\(name)")
             ]
         ),
     ],
@@ -79,3 +63,44 @@ let project = Project(
         "README.md"
     ]
 )
+
+private func debugSettings() -> SettingsDictionary {
+    let settings = baseSettings
+    return settings
+}
+
+private func stagingSettings() -> SettingsDictionary {
+    let settings = baseSettings
+    return settings
+}
+
+private func releaseSettings() -> SettingsDictionary {
+    let settings = baseSettings
+    return settings
+}
+
+private func getBuildableFolders() -> [BuildableFolder] {
+    [
+        .init(stringLiteral: "\(name)/Sources"),
+        .init(stringLiteral: "\(name)/Resources")
+    ]
+}
+
+private func getBasicSettings() -> SettingsDictionary {
+    [
+        "CURRENT_PROJECT_VERSION": "0",
+        "MARKETING_VERSION": "0.0.0",
+        "DEVELOPMENT_TEAM": "WMB8HCJXS9",
+        "ENABLE_USER_SCRIPT_SANDBOXING": "YES",
+        "STRING_CATALOG_GENERATE_SYMBOLS": "NO",
+        "ENABLE_ASSET_SYMBOL_GENERATOR": "YES",
+        "ENABLE_STRING_CATALOG_SYMBOLS": "YES"
+    ]
+}
+
+private func getExtenseInfoPlist() ->  [String : Plist.Value] {
+    [
+        "CFBundleDisplayName": "$(APP_DISPLAY_NAME)",
+        "UILaunchStoryboardName": "LaunchScreen"
+    ]
+}
