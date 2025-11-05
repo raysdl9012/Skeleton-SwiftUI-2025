0fa3ea3347f76ec3e4541178a9f52dd6af8e9f4f
Reinner Steven Daza Leiva <reinner.leiva@outlook.com>
2025-11-05 10:31:06 -0500

    Initial Template

---
+++ Project.swift
@@ -0,0 +1,76 @@
+import Foundation
+import ProjectDescription
+
+// MARK: - Base Settings
+
+private let name = "Project"
+private let bundleIdentifier = "com.rsdl.project"
+private let organization = "rsdl"
+
+let baseSettings: SettingsDictionary = [:]
+
+func debugSettings() -> SettingsDictionary {
+    var settings = baseSettings
+    settings["ENABLE_TESTABILITY"] = "YES"
+    return settings
+}
+
+func stagingSettings() -> SettingsDictionary {
+    var settings = baseSettings
+    return settings
+}
+
+func releaseSettings() -> SettingsDictionary {
+    var settings = baseSettings
+    return settings
+}
+
+// MARK: - Project Definition
+let project = Project(
+    name: "\(name)",
+    organizationName: "\(organization)",
+    settings: .settings(
+        configurations: [
+            .debug(name: "Debug", settings: debugSettings(), xcconfig: "InstagramClone/Configuration/xcconfigs/Debug.xcconfig"),
+            .debug(name: "Staging", settings: stagingSettings(), xcconfig: "InstagramClone/Configuration/xcconfigs/Staging.xcconfig"),
+            .release(name: "Release", settings: releaseSettings(), xcconfig: "InstagramClone/Configuration/xcconfigs/Release.xcconfig")
+        ]
+    ),
+    targets: [
+        .target(
+            name: "\(name)",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "\(bundleIdentifier)",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            buildableFolders: [
+                "Project/Sources",
+                "Project/Resources",
+            ],
+            dependencies: []
+        ),
+        .target(
+            name: "ProjectTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "\(bundleIdentifier)Tests",
+            infoPlist: .default,
+            buildableFolders: [
+                "Project/Tests"
+            ],
+            dependencies: [
+                .target(name: "Project")
+            ]
+        ),
+    ],
+    additionalFiles: [
+        "README.md"
+    ]
+)
